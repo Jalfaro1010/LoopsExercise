aac85626ce83de2e56626502b2c3eb928826f259
Jalfaro1010 <Joshua.alfaro@gmail.com>
2021-10-11 19:16:30 -0400

    Created and Completed Exercise

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1
+for (i = 1; i <= 7; i++) {
+    console.log(i);
+}
+
+// 2
+for (i = 1; i <= 25; i += 4){
+    console.log(i);
+}
+
+// 3 A
+const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+];
+
+// 3 B
+for (i = 0; i < wizards.length; i++){
+    console.log(`${i + 1}. ${wizards[i]}`);
+}
+
+// 4 A
+let harryPotterMovies = [0];
+
+// 4 B
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
+
+// 4 C
+console.log(harryPotterMovies);
+
+
+    
